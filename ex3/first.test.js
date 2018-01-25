9cfd10fe5ad62095e925ec678ee3f1a177887a1b
parth <srivastavaparth2016@gmail.com>
2018-01-25 14:27:21 +0530

    adding test case 2 for ex3

--- ex3/first.test.js
+++ ex3/first.test.js
@@ -6,4 +6,7 @@ describe('defining test case first ', () => {
   it('test case 1', () => {
     expect(read(filelocation)).toBe(5);
   });
+  it('test case 2', () => {
+    expect(read('/Users/parthsrivastava/worknode/ex3/file0.txt')).toBe(0);
+  });
 });
