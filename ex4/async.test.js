8832a319cc866027ec322540a7dd073c0d095dd5
parth <srivastavaparth2016@gmail.com>
2018-01-27 18:07:12 +0530

    test 3 for async.jw

--- ex4/async.test.js
+++ ex4/async.test.js
@@ -5,7 +5,10 @@ describe('defining file path ', () => {
   it('test case 1 for null being passed', () => {
     expect(read(null)).toBe(false);
   });
-  it('test case 1 for null being passed', () => {
+  it('test case 2 for nothing being passed', () => {
     expect(read()).toBe(false);
+  });
+  it('test case 3 for undefined being passed', () => {
+    expect(read(undefined)).toBe(false);
   });
 });
