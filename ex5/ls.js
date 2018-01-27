527dab5d6f7bbae776c4ad3a949a5834f7e4a59a
parth <srivastavaparth2016@gmail.com>
2018-01-27 20:13:25 +0530

    ls.js

--- ex5/ls.js
+++ ex5/ls.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
 
-function callback(err, list) {
-
-}
-fs.readdir(pathname, callback);
+const callback = (err, list) => {
+  if (err) {
+    console.log(err);
+  }
+  arr = [];
+  list.map((element) => {
+    if (element.indexOf(process.argv[3]) > 0) {
+      arr.push(element);
+    }
+    return 0;
+  });
+  arr.forEach((item) => {
+    console.log(item);
+  });
+};
+fs.readdir(process.argv[2], callback);
