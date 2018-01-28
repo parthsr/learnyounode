5033109fa90bb326e4100ad7461ac0446b1d16bb
parth <srivastavaparth2016@gmail.com>
2018-01-29 01:10:18 +0530

    changes in tcp.js

--- ex10/tcp.js
+++ ex10/tcp.js
@@ -1,10 +1,20 @@
 const net = require('net');
 
+const check = (input) => {
+  if (input <= 9) {
+    return `0${input}`;
+  }
+  return input;
+};
 const listener = (socket) => {
   const date = new Date();
-  currentHours = date.getHours();
-  currentHours = (`0${currentHours}`).slice(-2);
-  socket.end(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${currentHours}:${date.getMinutes()}`);
+  const year = date.getFullYear();
+  const month = check(date.getMonth() + 1);
+  const day = check(date.getDate());
+  const hour = check(date.getHours());
+  const min = check(date.getMinutes());
+  socket.write(`${year}-${month}-${day} ${hour}:${min}\n`);
+  socket.end();
 };
 const server = net.createServer(listener);
 server.listen(process.argv[2]);
