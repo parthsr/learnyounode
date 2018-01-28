d3b777facd458136d45a448c46850ffe69dd907c
parth <srivastavaparth2016@gmail.com>
2018-01-28 23:40:31 +0530

    adding http.js

--- ex7/http.js
+++ ex7/http.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
 const callback = (response) => {
+  response.setEncoding('utf8');
   response.on('data', (data) => {
-    console.log(data.toString());
+    console.log(data);
   });
 };
-
 http.get(process.argv[2], callback);
